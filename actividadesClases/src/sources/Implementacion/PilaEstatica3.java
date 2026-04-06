1148a78ff72684545eb5dfb8dc87b8ba98b84375
Lichi3006 <129699825+Lichi3006@users.noreply.github.com>
2026-04-06 17:55:31 -0300

    Refactor PilaEstatica3 class for cleaner code
    Refactor PilaEstatica3 to remove unnecessary 'this' keyword.

--- actividadesClases/src/sources/Implementacion/PilaEstatica3.java
+++ actividadesClases/src/sources/Implementacion/PilaEstatica3.java
@@ -2,36 +2,40 @@ package sources.Implementacion;
 
 import sources.Interfaces.PilaTDA;
 
+/**
+ * Esta implementación de pila tiene su tope en el último valor indexado del array, por lo que se apila al final y se elimina por soft delete, reduciendo el contador.
+ * Posee una complejidad matematica de O(1)
+ */
+
+
 public class PilaEstatica3 implements PilaTDA {
     private static final int MAX = 100;
     private int[] datos;
     private int cantidad;
 
     public void InicializarPila() {
-        this.datos = new int[100];
-        this.cantidad = 0;
+        datos = new int[100];
+        cantidad = 0;
     }
 
     public void Apilar(int x) {
-        if (this.cantidad < 100) {
-            this.datos[this.cantidad] = x;
-            ++this.cantidad;
+        if (cantidad < 100) {
+            datos[cantidad] = x;
+            cantidad++;
         }
-
     }
 
     public void Desapilar() {
-        if (!this.PilaVacia()) {
-            --this.cantidad;
+        if (!PilaVacia()) {
+            cantidad--;
         }
-
     }
 
     public int Tope() {
-        return this.datos[this.cantidad - 1];
+        return datos[cantidad - 1];
     }
 
     public boolean PilaVacia() {
-        return this.cantidad == 0;
+        return cantidad == 0;
     }
 }
