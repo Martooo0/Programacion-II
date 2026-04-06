ee36375f7decd8498069ae580114a4fa8762ab80
Lichi3006 <129699825+Lichi3006@users.noreply.github.com>
2026-04-06 17:38:52 -0300

    Refactor PilaEstatica2 to simplify code
    Refactor PilaEstatica2 to remove 'this' keyword for clarity.

--- actividadesClases/src/sources/Implementacion/PilaEstatica2.java
+++ actividadesClases/src/sources/Implementacion/PilaEstatica2.java
@@ -2,37 +2,43 @@ package sources.Implementacion;
 
 import sources.Interfaces.PilaTDA;
 
+/**
+ * Esta implementación de pila utiliza el primer valor del array como el contador, por lo que cada vez que se utiliza datos[datos[0]] se está acudiendo al dato del índice posterior al último ingresado 
+ * Posee una complejidad matematica de O(1), la ventaja es la ausencia de necesidad de una variable externa, avogando por un uso menor de memoria, la desventaja es que rompe el que viola el principio "KISS" 
+ */
+
+
 public class PilaEstatica2 implements PilaTDA {
     private int[] datos;
 
     @Override
     public void InicializarPila() {
-        this.datos = new int[101]; 
-        this.datos[0] = 0;
+        datos = new int[101]; 
+        datos[0] = 0;
     }
 
     @Override
     public void Apilar(int x) {
-        if (this.datos[0] < 100) {
-            this.datos[0]++;
-            this.datos[this.datos[0]] = x;
+        if (datos[0] < 100) {
+            datos[0]++;
+            datos[datos[0]] = x;
         }
     }
 
     @Override
     public void Desapilar() {
-        if (!this.PilaVacia()) {
-            this.datos[0]--;
+        if (!PilaVacia()) {
+            datos[0]--;
         }
     }
 
     @Override
     public int Tope() {
-        return this.datos[this.datos[0]];
+        return datos[datos[0]];
     }
 
     @Override
     public boolean PilaVacia() {
-        return this.datos[0] == 0;
+        return datos[0] == 0;
     }
 }
