f09f491ab5adff56073f3b2bf517e5ac0b01ab54
Lichi3006 <129699825+Lichi3006@users.noreply.github.com>
2026-04-06 18:05:26 -0300

    Refactor comments and fix Tope method return value
    Updated comments for clarity and improved explanation of the stack implementation. Fixed the return value in the Tope method to reflect the correct top element index.

--- actividadesClases/src/sources/Implementacion/PilaEstatica2.java
+++ actividadesClases/src/sources/Implementacion/PilaEstatica2.java
@@ -3,12 +3,18 @@ package sources.Implementacion;
 import sources.Interfaces.PilaTDA;
 
 /**
- * Esta implementación de pila utiliza el primer valor del array como el contador, por lo que cada vez que se utiliza datos[datos[0]] se está acudiendo al dato del índice posterior al último ingresado 
- * El tope se encuentra en el último índice del array (siendo la posición [datos[0] y no [datos[0]-1 debido que al apilar primero se aumenta el contador y luego se inserta)
- * Posee una complejidad matematica de O(1), la ventaja es la ausencia de necesidad de una variable externa, avogando por el menor uso posible de memoria, la desventaja es que rompe el principio "KISS" 
+ * Implementación estática de una Pila (TDA) de complejidad O(1) basada en "Señalización en banda".
+ * * Estrategia de diseño:
+ * Utiliza el índice [0] del propio arreglo para almacenar el contador de elementos (estado), 
+ * reservando los índices del [1] en adelante para los datos reales. El tope de la pila 
+ * siempre se encuentra apuntado por el valor almacenado en datos[0].
+ * * Ventajas:
+ * Ausencia de variables de estado externas, abogando por un uso estructuralmente mínimo 
+ * de la memoria.
+ * * Desventajas:
+ * Rompe el principio "KISS" y el de Separación de Responsabilidades, ya que el arreglo 
+ * mezcla metadatos de control con datos de usuario, reduciendo radicalmente la legibilidad.
  */
-
-
 public class PilaEstatica2 implements PilaTDA {
     private int[] datos;
 
@@ -21,8 +27,8 @@ public class PilaEstatica2 implements PilaTDA {
     @Override
     public void Apilar(int x) {
         if (datos[0] < 100) {
-            datos[0]++;
             datos[datos[0]] = x;
+            datos[0]++;
         }
     }
 
@@ -35,7 +41,7 @@ public class PilaEstatica2 implements PilaTDA {
 
     @Override
     public int Tope() {
-        return datos[datos[0]];
+        return datos[datos[0]]-1;
     }
 
     @Override
